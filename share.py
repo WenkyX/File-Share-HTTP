bed3909e81afcd56df670942eec744e12393e068
WenkyX <191382721+WenkyX@users.noreply.github.com>
2025-05-01 17:37:14 +0700

    improve ui, add get progress endpoint for zip progress

--- share.py
+++ share.py
@@ -11,7 +11,8 @@ import cgi
 import json
 import tkinter as tk
 from tkinter import filedialog
-import threading
+import threading, uuid
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-port", type=int, help="Port number")
@@ -20,6 +21,8 @@ parser.add_argument("-path", type=str, help="Path to directory")
 args = parser.parse_args()
 
 #TODO make zip progress job so user know when they download as zip
+
+zip_progress = {}
 
 def get_local_ip():
     try:
@@ -36,7 +39,7 @@ def get_local_ip():
 # print(f"Path: {args.path}")
 
 PORT = args.port or 8700
-DIRECTORY_TO_SERVE = args.path or "/opt"
+DIRECTORY_TO_SERVE = args.path or "./"
 BASE_DIR = os.path.abspath(DIRECTORY_TO_SERVE)
 os.chdir(DIRECTORY_TO_SERVE)
 HTML_TEMPLATE = os.path.join(os.path.dirname(__file__), "index.html")
@@ -57,7 +60,7 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
         log_output(message1)
     def do_GET(self):
         global curpath
-        if self.path.endswith('/folder.svg') or self.path.endswith('/unknown.svg') or self.path.endswith('/upload.svg'):
+        if self.path.endswith('/folder.svg') or self.path.endswith('/unknown.svg') or self.path.endswith('/upload.svg') or self.path.endswith('/alert.svg'):
             # Serve the SVG files from the directory where the Python script is located
             file_path = os.path.join(os.path.dirname(__file__), os.path.basename(self.path))
             # print(file_path)
@@ -76,20 +79,51 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
         elif os.path.isdir(self.path.lstrip('/').replace('%20', ' ')):  # Check if path is a directory
             curpath = os.path.join(BASE_DIR, self.path.lstrip('/'))
             return self.serve_directory(self.path + "/")  # Serve subdirectory
+        
         elif self.path.startswith('/download_zip'):
             query_components = parse_qs(urlparse(self.path).query)
             file_name = query_components.get("file", [None])[0]
+            print(file_name)
 
             if file_name:
-                zip_file = self.create_zip([file_name])
+                # zip_file = self.create_zip([file_name])
+
+                job_id = str(uuid.uuid4())
+                zip_progress[job_id] = 0
+                threading.Thread(target=self.create_zip_async, args=(job_id, [file_name])).start()
+
                 self.send_response(200)
-                self.send_header("Content-Type", "application/zip")
-                self.send_header("Content-Disposition", f"attachment; filename={file_name}.zip")
-                self.send_header("Content-Length", str(len(zip_file.getvalue())))
+                # self.send_header("Content-Type", "application/zip")
+                # self.send_header("Content-Disposition", f"attachment; filename={file_name}.zip")
+                # self.send_header("Content-Length", str(len(zip_file.getvalue())))
                 self.end_headers()
-                self.wfile.write(zip_file.getvalue())
+                # self.wfile.write(zip_file.getvalue())
+                self.wfile.write(job_id.encode())
             else:
                 self.send_error(400, "Bad Request: No file specified")
+
+        elif self.path.startswith('/zip_progress'):
+            query = parse_qs(urlparse(self.path).query)
+            job_id = query.get("id", [None])[0]
+            progress = zip_progress.get(job_id)
+            print("=====================================================")
+            print(zip_progress)
+            print(progress)
+            print("=====================================================")
+            if progress is None:
+                self.send_error(404)
+            elif isinstance(progress, tuple) and progress[0] == "done":
+                self.send_response(200)
+                self.send_header("Content-Type", "application/zip")
+                self.send_header("Content-Disposition", "attachment; filename=download.zip")
+                self.send_header("Content-Length", str(len(progress[1].getvalue())))
+                self.end_headers()
+                self.wfile.write(progress[1].getvalue())
+            else:
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(str(progress).encode())
+
         elif self.path.startswith('/get-file-size'):
             query_components = parse_qs(urlparse(self.path).query)
             folder = query_components.get("path", [None])[0]
@@ -184,19 +218,29 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
             bytes /= 1024
         return f"{bytes:.2f} PB"
 
-    def create_zip(self, items):
+    def create_zip_async(self, job_id, items):
+        # total_steps = len(items)
+        total_count = 0
+        i = 0
+        # print(total_steps, "total_stepstotal_stepstotal_stepstotal_stepstotal_stepstotal_stepstotal_stepstotal_steps")
         zip_buffer = io.BytesIO()
         with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zipf:
-            for item in items:
+            for i, item in enumerate(items):
                 item = unquote(item).strip("/")
                 item_path = os.path.join(os.getcwd(), item)
+                
                 base_dir = os.path.basename(item)  # Get just the final part (like "c")
 
                 if os.path.isfile(item_path):
                     zipf.write(item_path, arcname=os.path.basename(item_path))
                 elif os.path.isdir(item_path):
                     for root, _, files in os.walk(item_path):
+                        total_count += len(files)
+                        print(total_count, "total_counttotal_counttotal_counttotal_counttotal_counttotal_counttotal_count")
+                    for root, _, files in os.walk(item_path):
                         for file in files:
+                            i += 1
+                            zip_progress[job_id] = int((i + 1) / total_count * 100)
                             full_path = os.path.join(root, file)
                             # Make arcname relative to the base_dir
                             relative_path = os.path.relpath(full_path, start=os.path.dirname(item_path))
@@ -204,7 +248,18 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
                 else:
                     print(f"Warning: {item_path} does not exist or is not a valid file or directory.")
         zip_buffer.seek(0)
-        return zip_buffer
+        zip_progress[job_id] = "done", zip_buffer
+        # return zip_buffer
+    
+    # def create_zip_async(job_id, files):
+    #     total_steps = len(files)
+    #     zip_buffer = io.BytesIO()
+    #     with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    #         for i, file in enumerate(files):
+    #             zipf.writestr(file, f"Contents of {file}")
+    #             zip_progress[job_id] = int((i + 1) / total_steps * 100)
+    #     zip_buffer.seek(0)
+    #     zip_progress[job_id] = "done", zip_buffer
 
     def serve_directory(self, path):
         """Serve the directory content with an index.html if available"""
@@ -276,6 +331,10 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
             icon_href = "folder.svg" if os.path.isdir(name) else "unknown.svg"
             list_items += f"""
             <li class="files">
+                <div class="buffering">
+                    <div class="spinner"></div>
+                    <img src="alert.svg">
+                </div>
                 <a class="file" href="{href}" id="{display_name}">
                     <div class=icon>
                         <img src="{icon_href}">
@@ -298,34 +357,38 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(encoded)
 
 
+server_instance = None
+
 if __name__ == '__main__':
     def start_server():
+        global server_instance
         try:
             socketserver.TCPServer.allow_reuse_address = True
-            with socketserver.ThreadingTCPServer(("", PORT), CustomHandler) as httpd:
-                httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                localIp = get_local_ip()
-                if localIp == "Error":
-                    log_output(f"Serving {DIRECTORY_TO_SERVE} at 127.0.0.0 instead with Port {PORT}")
-                    log_output(f"Open http://127.0.0.0:{PORT} in your browser")
-                else:
-                    log_output(f"Local IP: {localIp}")
-                    log_output(f"Serving {DIRECTORY_TO_SERVE} at port {PORT}")
-                    log_output(f"Open http://{localIp}:{PORT} in your browser")
-                    # label.config(text=f"Server started on port {PORT}")
+            server_instance = socketserver.ThreadingTCPServer(("", PORT), CustomHandler)
+            server_instance.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            localIp = get_local_ip()
+            if localIp == "Error":
+                log_output(f"Serving {DIRECTORY_TO_SERVE} at 127.0.0.0 instead with Port {PORT}")
+                log_output(f"Open http://127.0.0.0:{PORT} in your browser")
+            else:
+                log_output(f"Local IP: {localIp}")
+                log_output(f"Serving {DIRECTORY_TO_SERVE} at port {PORT}")
+                log_output(f"Open http://{localIp}:{PORT} in your browser")
+                # label.config(text=f"Server started on port {PORT}")
 
-                # print('testttttttttt')
-                # print(CustomHandler.format_size(CustomHandler.get_folder_size(DIRECTORY_TO_SERVE)))
-                # print('bruh')
+            # print('testttttttttt')
+            # print(CustomHandler.format_size(CustomHandler.get_folder_size(DIRECTORY_TO_SERVE)))
+            # print('bruh')
 
-                try:
-                    httpd.serve_forever()
-                except KeyboardInterrupt:
-                    log_output("\nShutting down...")
-                    httpd.shutdown()
-                    httpd.server_close()
+            try:
+                server_instance.serve_forever()
+            except KeyboardInterrupt:
+                log_output("\nShutting down...")
+                server_instance.shutdown()
+                server_instance.server_close()
         except Exception as e:
             log_output(e)
+            return e
     
     def log_output(message):
         message = str(message)
@@ -350,6 +413,14 @@ if __name__ == '__main__':
             return e
         server_thread = threading.Thread(target=start_server, daemon=True)
         server_thread.start()
+
+    def stop_server():
+        global server_instance
+        if server_instance:
+            server_instance.shutdown()
+            server_instance.server_close()
+            log_output("Server stopped.")
+            server_instance = None
     
     def choose_folder():
         folder = filedialog.askdirectory()
@@ -368,7 +439,7 @@ if __name__ == '__main__':
     label.pack(side=tk.LEFT)
 
     pathInput = tk.Entry(frame)
-    pathInput.insert(tk.END, "./")
+    pathInput.insert(tk.END, "../")
     pathInput.pack(side=tk.LEFT, fill=tk.X, expand=True)
     # pathInput.place(x = 30, y = 10)
 
@@ -380,9 +451,13 @@ if __name__ == '__main__':
     portInput.insert(tk.END, "8701")
     portInput.pack(pady=10)
 
-    button = tk.Button(root, text="Click Me", command=on_click)
+    button = tk.Button(root, text="Start Server", command=on_click)
     button.pack(pady=10)
 
+    stopButton = tk.Button(root, text="Stop Server", command=stop_server)
+    stopButton.pack(pady=10)
+
+    #TODO make a maximum limit
     log_text = tk.Text(root, height=600, width=600, wrap=tk.WORD, state=tk.NORMAL)
     log_text.pack(padx=10, pady=10)
 
